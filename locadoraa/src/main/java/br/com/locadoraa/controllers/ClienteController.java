48e9c477cc7b6260974f39ae56749f2637396e6c
Miguel <miguel.amaral.sales@gmail.com>
2024-08-14 16:10:48 -0300

    update
    add exception if have the same cp

--- locadoraa/src/main/java/br/com/locadoraa/controllers/ClienteController.java
+++ locadoraa/src/main/java/br/com/locadoraa/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.locadoraa.controllers;
 import br.com.locadoraa.model.Cliente;
 import br.com.locadoraa.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,14 +23,17 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+    public ResponseEntity<String> create(@RequestBody Cliente cliente) {
         try {
             Cliente savedCliente = clienteService.save(cliente);
-            return ResponseEntity.status(HttpStatus.CREATED).body(savedCliente);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Cliente criado com sucesso: " + savedCliente.getCpf());
+        } catch (DataIntegrityViolationException e) {
+            // Retorna um erro 409 (Conflict) quando já existe um cliente com o CPF
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         } catch (Exception e) {
             // Log the error and return a 500 status code
             e.printStackTrace(); // For debugging purposes
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao criar cliente.");
         }
     }
 
@@ -41,7 +45,7 @@ public class ClienteController {
         } catch (Exception e) {
             // Log the error and return a 500 status code
             e.printStackTrace(); // For debugging purposes
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
--- locadoraa/src/main/java/br/com/locadoraa/services/ClienteService.java
+++ locadoraa/src/main/java/br/com/locadoraa/services/ClienteService.java
@@ -3,6 +3,7 @@ package br.com.locadoraa.services;
 import br.com.locadoraa.model.Cliente;
 import br.com.locadoraa.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,9 @@ public class ClienteService {
     private ClienteRepository clienteRepository;
 
     public Cliente save(Cliente cliente) {
+        if (clienteRepository.existsById(cliente.getCpf())) {
+            throw new DataIntegrityViolationException("Já existe um cliente com o CPF: " + cliente.getCpf());
+        }
         return clienteRepository.save(cliente);
     }
 
